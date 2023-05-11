a22021bed812fb756b29a303c0efac097d015026
MikaelUlvesjo <mikael@ulvesjo.com>
2023-05-11 11:07:43 +0200

    Update priceBasedOnOff.js
    Changed scheduling to always run on even divisions of the updateTime, so if set to 15 minutes (900 sec) it will run xx:00, xx:15, xx:30 and xx:45.
    
    updateTime is now in seconds was missiseconds.
    Color mode now uses allwaysOnMaxPrice and allwaysOffMinPrice.

--- priceBasedOnOff.js
+++ priceBasedOnOff.js
@@ -1,14 +1,16 @@
 let CONFIG = {
     priceApiEndpoint: "https://www.elprisetjustnu.se/api/v1/prices/",   // see https://www.elprisetjustnu.se/elpris-api
-    tomorowsPricesAfter: 14, //it will get tomorrows prices and if the time is after 14 for 14:00
+    tomorowsPricesAfter: 15, //it will get tomorrows prices and if the time is after 14 for 14:00
     timezone: 1, //in positive or negative value e.g: 1 for CET or -6 for CST
     daylightSaving: true,//boolean, if true and date is after last Sunday in March and Before last Sunday in October 1 hour weill be added to timezone.
     zone: "SE4", // SE1,SE2,SE3 or SE4
     inUseLimit: 5.0, // nr of wats required to consider the controlled unit to be running and to not swith it of for non pm units set this to -1.0 
-    updateTime: 300000, // 5 minutes. Price update interval in milliseconds
+    updateTime: 900, // 15 minutes. Price update interval in seconds
     switchId: 0, // the id of the switch starts at 0
-    allwaysOnMaxPrice: 1.3, // SEK/kWh if the price is below or equal this value the switch should be on no matter if checkNextXHours would turn it off (price without tax or other fees)
-    allwaysOffMinPrice: 3.0, // SEK/kWh if the price is above or equal this value the switch should be off no matter if allwaysOnHours would turn it on (price without tax or other fees)
+    allwaysOnMaxPrice: 0.5, // SEK/kWh if the price is below or equal this value the switch should be on no matter if checkNextXHours would turn it off (price without tax or other fees) 
+    // and in color mode the first color will be used
+    allwaysOffMinPrice: 2.0, // SEK/kWh if the price is above or equal this value the switch should be off no matter if allwaysOnHours would turn it on (price without tax or other fees)
+    // and in color mode the last color will be used
     allwaysOnHours: [{ from: 21, to: 23 }], //Time spans when allways on format [{from: 8, to:8},{from: 20, to:23}] to have it on from 8:00-8:59 and 20:00 to 23:59
     onOffLimit: 1.0, // is used to set the price limit where to turn on and of switch
     //so if current price > (avg price * onOffLimit)  then turn off
@@ -20,9 +22,10 @@ let CONFIG = {
     // if stopAtDataEnd is true will only check current days values and if it passes the end of the data it will stop checking more values,
     invertSwitch: false, // invert the switch action. Set inUseLimit: -1.0 to use this.
     debugMode: true, // Set to false to enable switching of power.
-    switchMode: false, // Set to true to swith power on and of based on price
-    colorMode: false, // Set to true to change color on shelly plus plug s led from green to red based on price. Lowest price of the day will be green and highest price of the day will be red
-    colors: ["0,100,0", "100,100,0", "100,0,100", "100,0,0"], //Colors used for shelly plus plug s led, default 0%-25%: green, 25%-50%: yellow, 50%-75%: purple and 75%-100%: red
+    switchMode: false, // Set to true to switch power on and of based on price
+    colorMode: true, // Set to true to change color on shelly plus plug s led from green to red based on price. Lowest price of the day will be green and heighest price of the day will be red and 
+    colors: ["0,100,0", "100,100,0", "100,0,100", "100,0,0"], // (red, green ,blue from 0 to 100) Colors used for shelly plus plug s led
+    //Can be any number of colors where the first one is for the lowest price and the last for the max price.
 };
 let prices = [];
 let avg = null;
@@ -38,6 +41,19 @@ let nextAtemptToGetData = 0;
 
 function sendRequest(api, data, callback, userData) {
     Shelly.call(api, data, callback, userData);
+}
+
+function scheduleNextRun() {
+    print("current date: " + date.date);
+    let nextTimeToNextRun = (CONFIG.updateTime) - (((date.minute * 60) + date.second) % (CONFIG.updateTime));
+    let nextDate = epochToDate(date.epoch + nextTimeToNextRun, CONFIG.timezone, CONFIG.daylightSaving);
+    if (nextTimeToNextRun < (CONFIG.updateTime / 3) && date.hour === nextDate.hour) {
+        print("To close to next run will skipp that run");
+        nextTimeToNextRun = nextTimeToNextRun + CONFIG.updateTime;
+        nextDate = epochToDate(date.epoch + nextTimeToNextRun, CONFIG.timezone, CONFIG.daylightSaving);
+    }
+    print("Next run: " + nextDate.date);
+    Timer.set(nextTimeToNextRun * 1000, false, start);
 }
 
 function start() {
@@ -61,6 +77,7 @@ function processCurrentDate(response, errorCode, errorMessage) {
         return;
     }
     date = epochToDate(response.unixtime, CONFIG.timezone, CONFIG.daylightSaving);
+    scheduleNextRun();
     getCurrentUsage();
 }
 
@@ -78,8 +95,9 @@ function processCurrentUsageResponse(response, errorCode, errorMessage) {
     }
     currentSwitchState = response.output;
     if (CONFIG.inUseLimit >= 0) {
-        powerUsage = response.apower;
         date = epochToDate(response.aenergy.minute_ts, CONFIG.timezone, CONFIG.daylightSaving);
+        scheduleNextRun();
+        powerUsage = response.apower;
     } else {
         powerUsage = 0.0;
     }
@@ -172,6 +190,7 @@ function processCurrentPriceResponse(response, errorCode, errorMessage, userdata
         getCurrentPrice(24);
         return;
     }
+    print(date.date + ": Hour: " + JSON.stringify(date.hour) + ", current price: " + JSON.stringify(prices[date.hour]) + " SEK/kWh, min price today: " + JSON.stringify(min) + " SEK/kWh, max price today: " + JSON.stringify(max) + " SEK/kWh, avg price today: " + JSON.stringify(avg) + " SEK/kWh, always on limit: " + JSON.stringify(CONFIG.allwaysOnMaxPrice) + " SEK/kWh, always of limit: " + JSON.stringify(CONFIG.allwaysOffMinPrice) + " SEK/kWh");
     setColor();
     switchOnOrOff();
 }
@@ -248,11 +267,19 @@ function setColor() {
         let interval = 100 / CONFIG.colors.length;
         let color = "0,0,100";
         for (let i = 0; i < CONFIG.colors.length; i++) {
-            if (percent >= (i * interval) ) {
+            if (percent >= (i * interval)) {
                 color = CONFIG.colors[i];
             }
         }
-        print("Setting color to rgb[" + color + "]");
+        if (prices[date.hour] <= CONFIG.allwaysOnMaxPrice) {
+            color = CONFIG.colors[0];
+            print("Price below allwaysOnMaxPrice, Setting color to rgb[" + color + "]");
+        } else if (prices[date.hour] >= CONFIG.allwaysOffMinPrice) {
+            color = CONFIG.colors[CONFIG.colors.length - 1];
+            print("Price above allwaysOffMinPrice, Setting color to rgb[" + color + "]");
+        } else {
+            print("Setting color to rgb[" + color + "]");
+        }
         sendRequest(
             "PLUGS_UI.SetConfig",
             "{\"config\": {\"leds\": {\"night_mode\": { \"active_between\": [\"21:00\", \"07:00\"], \"brightness\": 5, \"enable\": true },\"colors\": {\"power\": { \"brightness\": 25 }," +
@@ -336,6 +363,3 @@ function epochToDate(epochTimeIn, timezone, daylightSavingTime) {
 }
 
 start();
-Timer.set(CONFIG.updateTime, true, function (userdata) {
-    start();
-});
